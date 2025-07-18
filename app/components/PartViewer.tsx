0454858b19836415cf2003a91fe9c5dc421669ed
Carter Powell <cap4264@hotmail.com>
2025-07-17 21:39:50 -0400

    update

--- app/components/PartViewer.tsx
+++ app/components/PartViewer.tsx
@@ -1,27 +1,40 @@
 'use client'
 
-import React, { useMemo } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { geometriesFromScad } from '../components/scadParser'
-import { BufferGeometry } from 'three'
+import React, { useRef } from 'react'
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { STLLoader } from 'three-stdlib'
+import { BufferGeometry, MeshStandardMaterial, Mesh } from 'three'
 
-export default function PartViewer({ code }: { code: string }) {
-  const shapes = useMemo(() => geometriesFromScad(code), [code])
+type PartViewerProps = {
+  stlUrl: string
+}
+
+function STLModel({ stlUrl }: { stlUrl: string }) {
+  const geometry = useLoader(STLLoader, stlUrl)
+  const meshRef = useRef<Mesh>(null)
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.005
+    }
+  })
 
   return (
-    <div className="w-full h-[500px] bg-black rounded">
-      <Canvas>
-        <PerspectiveCamera makeDefault position={[150, 150, 150]} />
-        <ambientLight intensity={0.5} />
-        <pointLight position={[50, 50, 50]} />
-        <OrbitControls />
+    <mesh ref={meshRef} geometry={geometry as BufferGeometry}>
+      <meshStandardMaterial color="#357edd" metalness={0.5} roughness={0.3} />
+    </mesh>
+  )
+}
 
-        {shapes.map((geometry: BufferGeometry, idx: number) => (
-          <mesh key={idx} geometry={geometry}>
-            <meshStandardMaterial color="orange" />
-          </mesh>
-        ))}
+export default function PartViewer({ stlUrl }: PartViewerProps) {
+  return (
+    <div className="w-full h-[400px] border rounded bg-white">
+      <Canvas camera={{ position: [70, 70, 70], near: 0.1, far: 1000 }}>
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[50, 50, 50]} intensity={0.5} />
+        <OrbitControls enablePan={true} enableZoom={true} enableRotate={true} />
+        <STLModel stlUrl={stlUrl} />
       </Canvas>
     </div>
   )
